35a16d6741d23bcc5a15bcef2e7021e16e41e6b1
BhatiaDivtej <divtej17@connect.hku.hk>
2021-04-23 01:42:56 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
-
-
+//  Group Project of Group14_ENGG1340
+//  Game : GUESS THE MOVIE !
+//  Group Members:
+//  1. BHATIA Divtej Singh : 3035832438
+//  2. AGRAWAL Aryan : 3035812373
 
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 int main(){
-    
+
 }
