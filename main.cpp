6def06c219a3316862ce287e7205fa7fa79e1b57
BhatiaDivtej <divtej17@connect.hku.hk>
2021-04-23 02:27:20 +0530

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,17 +11,24 @@
 
 using namespace std;
 
+void print_movie(/*array containing movie name */){
+
+}
+
 
 int main(){
 
     //Introduction Output 
+    cout << endl;
+    cout << "***************************" << endl;
     cout << endl; //endline marking start of game
     cout << "ENGG1340 2020-21 Semester 2" << endl;
+    cout << endl;
     cout << "WELCOME TO GROUP PROJECT OF GROUP 14" << endl;
     cout << "Group Members" << endl;
     cout << "1. BHATIA Divtej Singh" << endl;
     cout << "2. AGRAWAL Aryan" << endl;
-    cout << "Press ENTER to Start" << endl;
+    cout << "Press ANY KEY to Start" << endl;
 
     cin.get(); // detecting enter
 
@@ -31,9 +38,47 @@ int main(){
     cout << "Press M for Multiplayer" << endl;
 
     char input_mode;
-    cin >> input_mode ; 
-    input_mode = toupper(input_mode); //even if lowercase is entered
+    cin >> input_mode ;
+    input_mode = toupper(input_mode); // for case when lowercase is entered
+    if (input_mode != 'S' || input_mode != 'M'){
+        cout << "Incorrect Choice, Play Again Soon !" << endl;
+        return 0;
+    }
+
+    //Using Fstream and Stringstream to Load a movie and hint
+
+    //Make a dynamic array of the size of the movie name 
+    //load hint string
+
+    //Unguessed contains the number of total alphabets to be guessed
+    int unguessed = 0;
+
+
+    //game starts:
+    cout << endl;
+    cout << "GUESS THE MOVIE ALPHABETS UNTIL YOU COMPLETE THE MOVIE NAME " << endl;
+
+    //SINGLEPLAYER Mode
+    if (input_mode == 'S'){
+        //enter Single Player Mode
+
+
+    }
 
 
 
+
+
+
+
+    //MULTIPLAYER MODE
+    else{
+        //enter Multiplayer Mode
+
+
+
+    }
+
+
+    return 0;
 }
