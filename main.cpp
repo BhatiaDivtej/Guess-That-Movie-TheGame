9dccbf4ae32cf8679dd58609582b209d1a6acd81
BhatiaDivtej <divtej17@connect.hku.hk>
2021-04-25 02:00:32 +0530

    Added Documentation
    I have added all the required documentation and required comments for each function and for if-else statements

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-
 //  Group Project of Group14_ENGG1340
 //  Game : GUESS THE MOVIE !
+
 //  Group Members:
 //  1. BHATIA Divtej Singh : 3035832438
 //  2. AGRAWAL Aryan : 3035812373
 
-//  Most Recent Error : single player does not work properly.
+//  Recent Comment : Game Works as expected ! Both Single and Multiplayer added.
 
 /* IMPLEMANTATION of Code Requirements :
 
@@ -15,7 +15,7 @@
 * Used For loop to loop through movie name file and stop at random number.
 
 2 : Data structures for storing game status:
-* Used Array Data structures to store game status 
+* Used Array Data structures to store game status (movie_display) 
 
 3 : Dynamic memory management
 * Created a dynamic array 
@@ -44,6 +44,9 @@
 using namespace std;
 
 
+//  This function Prints the movie to display
+//  All characters are printed in capital
+//  Unknown characters are printed as "_"
 
 void print_movie(int n ,char * movie_display){
     for ( int i = 0 ; i < n ; i ++ ){
@@ -54,12 +57,23 @@ void print_movie(int n ,char * movie_display){
 }
 
 
+
+//  This function takes user input for a guess character
+//  user input is case insensitive . 
+//  means a guess "a" is same as "A"
+//  done so that player need not enter in capital
+
 char get_guess(){
     char g ;
     cin >> g ;
-  //  g = toupper(g) ;
     return g;
 }
+
+
+//  This function checks if the user input is a correct guess 
+//  of the movie name 
+//  returns true for a correct guess
+//  returns false for a incorrect guess
 
 bool is_guess( char g , int n , char * movie, char * & movie_display){
     for (int i = 0 ; i < n ; i ++){
@@ -70,6 +84,12 @@ bool is_guess( char g , int n , char * movie, char * & movie_display){
     }
     return false;
 }
+
+
+
+//  This function updates the movie_display array to include the correct guess
+//  includes the correct guess
+//  stores the game status and player inputs
 
 void update_with_guess(char g, int n , char * movie , char * & movie_display,
    int & unguessed){
@@ -83,6 +103,10 @@ void update_with_guess(char g, int n , char * movie , char * & movie_display,
 
 }
 
+
+//  This function checks if the given character is a vowel.
+//  returns true if the vowel is a vowel
+
 bool isvowel(char x){
     if ( x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U' ||
   x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')
@@ -90,6 +114,13 @@ bool isvowel(char x){
     else
         return false;
 }
+
+
+
+//  This function checks if the guess was made previously 
+//  by the user
+//  prevents repeated guesses.
+//  returns true if user input is previously guessed by a player.
 
 bool previous_guess(int n ,char g , char * movie_display){
 	for (int i = 0 ; i < n ; i ++){
@@ -102,6 +133,13 @@ bool previous_guess(int n ,char g , char * movie_display){
 	return false;
 }
 
+
+
+//  The MAIN function of the game
+//  Contains the Output formatting and Input Requirents
+//  Contains the User Interface for 
+//  S = Single Player and 
+//  M = Multiplayer
 
 int main(){
 
@@ -186,16 +224,19 @@ int main(){
 
     for ( int i = 0 ; i < random_movie.length() ; i ++ ) { // finding the length
     //of the movie without spaces
-      if(random_movie[i] != ' ')
-        n += 1 ;
-      else num_of_words += 1 ;
+        if(random_movie[i] != ' '){
+            n += 1 ;
+
+        }
+        else {
+            num_of_words += 1 ;
+        }
     }
-
-
 
     int length = random_movie.length() ;
 
     // 3 : Making Dynamic Array :
+
     char * movie = new char[n];
     char * movie_display = new char[n];
 
@@ -204,10 +245,11 @@ int main(){
 
     for ( int i = 0 ; i < length ; i ++ ) {
         movie[i] = random_movie[i] ;
+
     }
 
 
-    //removing the consonants from the letter
+    //removing the consonants from the letter to hide from players
     for ( int i = 0 ; i < length; i ++){
         char q = movie[i];
         if ( isvowel(q) || isalpha(q) == 0 ){ // If there is a punctuation mark
@@ -227,14 +269,15 @@ int main(){
 
 
     //Unguessed contains the number of total alphabets to be guessed
-    int remaining_guess = 5 ;
+    int remaining_guess = 5 ; //the number of max incorrect guesses set to be 5.
 
-    //game starts:
+    //Game Starts:
     cout << endl;
     cout << "GUESS THE MOVIE ALPHABETS UNTIL YOU COMPLETE THE MOVIE NAME " << endl;
 
     //SINGLEPLAYER Mode
     if (input_mode == 'S'){
+        //Taking user input
         string player_name;
         cout << "Enter Player Name: " ;
         cin >> player_name;
@@ -266,9 +309,9 @@ int main(){
 			}
 			
 
-            if (is_guess(g, length, movie , movie_display) == false) {
+            if (is_guess(g, length, movie , movie_display) == false) {//wrong guess
 				cout << "INCORRECT! \n" << endl;
-                remaining_guess -= 1;
+                remaining_guess -= 1; //deduct available guess by 1
                 cout << "You now have "<< remaining_guess
                 <<" guess(es) remaining\n";
 
@@ -286,7 +329,7 @@ int main(){
 
             }
 
-			else{
+			else{ //case for correct output
 				cout << "CORRECT !!! Keep Going \n" << endl;
 			}
 			
@@ -294,7 +337,7 @@ int main(){
 
         }
 
-        if (unguessed == 0 ){
+        if (unguessed == 0 ){ //Movie is fully guessed
 			print_movie(length , movie_display);
 			cout << "Yes, the movie is ~~  ";
             cout << random_movie << endl ; // if player wins the completed movie
@@ -321,6 +364,7 @@ int main(){
     else if ( input_mode == 'M'){
         //enter Multiplayer Mode
 
+        //Taking Input/Output
 		string player1, player2;
 		cout << "ENTER NAME OF PLAYER 1: " ;
 		cin >> player1 ;
@@ -337,7 +381,13 @@ int main(){
 
 		int pp = 0 ; //for printing hint only once
 
-		while ( unguessed != 0 ){
+
+        //  Using a nested while loop to continue until the complete movie is guessed.
+        //  Interior while loops are for player streaks.
+        //  The player can continue to guess until he guesses incorrectly
+        //  The last player to guess the complete movie wins!
+
+		while ( unguessed != 0 ){ //movie contains unguessed letters
 
 			int chance1 = 1 ; //	chance1 is 0 if player 1 guesses incorrectly.
 			int chance2 = 1 ; //	chance2 is 0 if player 2 guesses incorrectly.
@@ -362,25 +412,28 @@ int main(){
 
 
 				//checking for a Legit Guess
-				if ( isvowel(g)){
+				if ( isvowel(g)){//if it is a vowel
 					cout << "~ Don't Guess Vowels as they are already visible! Try Again !\n" << endl;
 					continue;
 
 				}
 
-				if ( previous_guess(length, g , movie_display)){
+				if ( previous_guess(length, g , movie_display)){ //if same letter guessed before
 					cout << "~ This Guess has already been made !! Try Again !\n" <<endl ;
 					continue;
 
 				}
 
+                //Now checking the game conditions
 
-				if (is_guess(g, length, movie , movie_display ) == false){//case of incorrect guess
+                //case of incorrect guess
+
+				if (is_guess(g, length, movie , movie_display ) == false){
 					chance1 = 0;
 					cout << "Incorrect Guess ! \n" << endl;
 
 				}
-
+                
 
 				else{
 					update_with_guess(g, length , movie , movie_display, unguessed);
@@ -394,6 +447,7 @@ int main(){
 			}
 
 			//player2's turn 
+
 			while ( chance2 == 1 && unguessed != 0){
 				
 				print_movie(length , movie_display);
@@ -435,20 +489,24 @@ int main(){
 
 		}
 
-		
+		//Printing the movie name 
 		cout << "The movie was ~~  ";
         cout << random_movie << endl ;
 
     }
 
-    else{
+    // if "S" or "M" is not entered as a game mode choice input
+    else{ 
         cout << "INVALID CHOICE, Try Again Later" << endl;
     }
 
 	cout << "THANKS FOR PLAYING !\n" << endl ; 
 
+    //Deleting the dynamic memory allocated.
+
     delete [] movie;
     delete [] movie_display;
     return 0;
+    //Program ENDS
 
 }
